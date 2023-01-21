abeee3f2fb7ab85de8826e4e587fb8469e8f95d1
Lev1s <lev1sstudio@outlook.com>
2023-01-20 20:01:09 -0500

    init

---
+++ warn.py
@@ -0,0 +1,16 @@
+import argparse
+from pypushdeer import PushDeer
+
+## argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--message','-m',help='your message',default='⚠️')
+parser.add_argument('--desp','-d',help='desp message',default='⚠️')
+args = parser.parse_args()
+
+## get key
+keyfile = open('/root/pushkey')
+key = keyfile.read()
+key = key[:-1]
+
+pushdeer = PushDeer(pushkey=key)
+pushdeer.send_markdown(args.message,desp=args.desp)
---
+++ wecom_push.py
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   wecom_push.py
+@Time    :   2023/01/20 15:05:17
+@Author  :   Lev1s
+@Version :   1.0
+@Contact :   Lev1sStudio.cn@gmail.com
+@PW      :   http://Lev1s.cn
+@Github  :   https://github.com/o0Lev1s0o
+
+'''
+print('''
+    __             ___        _____ __            ___     
+   / /   ___ _   _<  /____   / ___// /___  ______/ (_)___ 
+  / /   / _ \ | / / / ___/   \__ \/ __/ / / / __  / / __ \\
+ / /___/  __/ |/ / (__  )   ___/ / /_/ /_/ / /_/ / / /_/ /
+/_____/\___/|___/_/____/   /____/\__/\__,_/\__,_/_/\____/
+''')
+# here put the import lib
+import sys
+sys.path.append("/root/scripts")
+
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
+SHA_TZ = timezone(
+    timedelta(hours=8),
+    name='Asia/Shanghai',
+)
+
+# 北京时间
+utc_now = datetime.utcnow().replace(tzinfo=timezone.utc)
+beijing_now = utc_now.astimezone(SHA_TZ)
+time = beijing_now.strftime("「%Y-%m-%d %H:%M:%S」")
+
+import wecom
+import argparse
+
+## argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--message','-m',help='your message',default='⚠️')
+args = parser.parse_args()
+
+CID = "ww996e2e704963f44b"
+APPID = "1000002"
+SECRET = "sHaEQCr8b9X1aWG9--ldiiOpAmTMDFrZ15oU_rhMPXE"
+
+ret = wecom.send_to_wecom(time+"\r\n"+args.message, CID, APPID, SECRET)
+print( ret )
